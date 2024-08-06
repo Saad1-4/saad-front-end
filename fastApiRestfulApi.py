a131ef33f0375e7309f4c19577b01a12600a252c
Saad1-4 <166531925+Saad1-4@users.noreply.github.com>
2024-08-06 09:27:14 +0100

    Add files via upload

---
+++ fastApiRestfulApi.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI , Query
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware,
+    allow_origins=["*"],  allow_methods=["*"],allow_headers=["*"],
+)
+class BMIOutput(BaseModel):
+    bmi: float
+    message: str
+
+@app.get("/")
+def Hi():
+    return {"message": "Marhaba python"}
+
+@app.get("/calculate_bmi")
+def calculate_bmi(
+    weight: float= Query(... , gt=20,lt=200, description="الوزن بالكيلوغرام") ,
+    height: float= Query(..., gt=1, lt=3, description="الطول بالمتر")
+    ):
+    bmi = (weight / (height ** 2))
+    if bmi < 18.5:
+        message = "لديك نقص في الوزن، كُل أكثر "
+    elif 18.5 <= bmi < 25:
+        message = "لديك وزن طبيعي، حافظ عليه."
+    elif 25 <= bmi < 30:
+        message = "لديك زيادة في الوزن، تمرن أكثر"
+    else:
+        message = "أنت تعاني من السمنة، قم بمراجعة طبيب"
+    return BMIOutput(bmi=bmi,message=message)
+    
+# link to calculate_bmi.py is : example: http://127.0.0.1:8000/calculate_bmi/?height=1.75&weight=70 output : {"bmi": 24.691358024691358,"message": "لديك وزن طبيعي، حافظ عليه."}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
